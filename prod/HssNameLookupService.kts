e4d5dc4b702857d4e731c29316c6b6a46ac669c5
Vihang Patil <vihang.patil@gmail.com>
2019-09-17 15:28:07 +0200

    Adding kts files.

---
+++ prod/HssNameLookupService.kts
@@ -0,0 +1,10 @@
+import org.ostelco.prime.storage.graph.HssNameLookupService
+
+object : HssNameLookupService {
+    override fun getHssName(regionCode: String): String {
+        return when (regionCode.toLowerCase()) {
+            "sg" -> "M1"
+            else -> "TEST"
+        }
+    }
+}
