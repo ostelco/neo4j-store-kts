bea89d5608e1f499fb1d43d5eb53a3d91f1bf392
Prasanth Ullattil <prasanth@telenordigital.com>
2019-10-10 15:39:04 +0200

    Adding more emails to the whitelist

--- dev/AllowedRegionsService.kts
+++ dev/AllowedRegionsService.kts
@@ -8,9 +8,17 @@ import org.ostelco.prime.storage.graph.PrimeTransaction
 
 object : AllowedRegionsService {
     override fun get(identity: Identity, customer: Customer, transaction: PrimeTransaction): Either<StoreError, Collection<String>> {
-        val allowedEmailDomains = listOf("@oya.sg", "@oya.world", "@redotter.sg", "@wgtwo.com", "havardnoren@gmail.com", "prasanth.u@gmail.com", "vihang.patil@gmail.com")
-        val matchedDomains = allowedEmailDomains.filter { customer.contactEmail.toLowerCase().endsWith(it) }
-        return if (matchedDomains.size > 0)
+        val allowedEmailSuffixes = listOf(
+                "@oya.sg",
+                "@oya.world",
+                "@redotter.sg",
+                "@wgtwo.com",
+                "havardnoren@gmail.com",
+                "prasanth.u@gmail.com",
+                "vihang.patil@gmail.com",
+                "rmz@rmz.no")
+        val matchedSuffixes = allowedEmailSuffixes.filter { customer.contactEmail.toLowerCase().endsWith(it) }
+        return if (matchedSuffixes.isNotEmpty())
             listOf("no", "sg", "us", "my").right()
         else
             listOf("sg", "us", "my").right()
