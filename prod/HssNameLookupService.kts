535904a639a62e18dcfac0bb337a8218e3c3c427
Vihang Patil <vihang.patil@gmail.com>
2019-11-05 10:37:36 +0100

    In prod, give 'Digi' profiles for 'my' region.

--- prod/HssNameLookupService.kts
+++ prod/HssNameLookupService.kts
@@ -4,6 +4,7 @@ object : HssNameLookupService {
     override fun getHssName(regionCode: String): String {
         return when (regionCode.toLowerCase()) {
             "sg" -> "M1"
+            "my" -> "Digi"
             "no" -> "Loltel"
             else -> "TEST"
         }
