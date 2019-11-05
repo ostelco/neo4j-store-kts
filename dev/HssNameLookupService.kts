995077ff6fe3dd7041a8283baf7aaadc1f7ac3a3
Vihang Patil <vihang.patil@gmail.com>
2019-11-05 10:37:06 +0100

    In dev, give 'loltel' profiles only for 'no' region.

--- dev/HssNameLookupService.kts
+++ dev/HssNameLookupService.kts
@@ -3,9 +3,8 @@ import org.ostelco.prime.storage.graph.HssNameLookupService
 object : HssNameLookupService {
     override fun getHssName(regionCode: String): String {
         return when (regionCode.toLowerCase()) {
-            "sg", "my" -> "TEST"
-            // "sg" -> "M1"
-            else -> "Loltel"
+            "no" -> "Loltel"
+            else -> "TEST"
         }
     }
 }
